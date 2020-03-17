a0ef0f2569cb2e9a402824355322e91ffd3d694d
Castrillon-San <60115630+Castrillon-San@users.noreply.github.com>
2020-03-17 10:30:41 -0500

    Add files via upload

---
+++ TrucoChar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = {"Natalia", "Samara", "Ronny"};
+
+            for(int i = 0; i < nombres.Length; i++)
+            {
+                string nombre = nombres[i];
+                for(int j = 0; j < nombre.Length; j++)
+                {
+                    char l = nombre[j];
+                    Console.WriteLine(l);
+                }
+            }
+            
+        }
+    }
+}
